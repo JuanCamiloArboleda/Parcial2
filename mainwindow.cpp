94acf96e360f18e3856e8912f324c2fb95bdf9df
Juan Camilo Arboleda <juan.arboleda16@udea.edu.co>
2021-09-20 21:27:49 -0500

    Cargar la imagen en una variable

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,18 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_imagenbtn_clicked()
+{
+    QString archivo = QFileDialog::getOpenFileName(this, tr("elije"),"", tr("images(*.png *.jpg *.jpeg *.bmp *.gif)"));
+    
+    if (QString::compare(archivo, QString()) != 0 ){
+        QImage image;
+        bool valido=image.load(archivo);
+        if  (valido){
+            image=image.scaledToWidth(ui->labelimage->width(),Qt::SmoothTransformation);
+            ui->labelimage->setPixmap(QPixmap::fromImage(image));
+        }
+    }
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QFileDialog>
+#include <iostream>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -14,6 +15,9 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_imagenbtn_clicked();
 
 private:
     Ui::MainWindow *ui;

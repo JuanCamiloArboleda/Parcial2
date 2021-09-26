a23a5aeef03cc08d36e8ee838f597a5623bdf7d4
dilan764 <62860859+dilan764@users.noreply.github.com>
2021-09-26 13:55:59 -0500

    submuestreo
    finalizacion del submuestreo con la implementacion de otros 2 for, permitiendonos reducir la cantidad de datos.

--- punto 4/main.cpp
+++ punto 4/main.cpp
@@ -1,43 +1,48 @@
 #include <iostream>
 #include <QImage>
+#include <fstream>
 using namespace std;
 
 int main()
 {
+
     string filename="../parcial2-intentosinqapplication/recibir";
     QImage im( filename.c_str());
     int altomatrix,anchomatrix;
-    int renglon=0,columna=0;
     int ancho=im.height(),alto=im.width(),R=0,G=0,B=0;
     cout<<"ingrese las dimensiones de la matriz: "<<endl;
     cout<<"altura de la matriz: "<<endl;
     cin>>altomatrix;
     cout<<"ancho de la matriz: "<<endl;
     cin>>anchomatrix;
-    int matrix[altomatrix][anchomatrix]; //matrix 16x16
+    int matrixrojo[altomatrix][anchomatrix],matrixazul[altomatrix][anchomatrix],matrixverde[altomatrix][anchomatrix]; //matrix 16x16
     int recuentoancho=0, recuentoalto=0;
     recuentoancho = ancho/anchomatrix;
     recuentoalto = alto/altomatrix;
-    for (int z=0;z<altomatrix*anchomatrix;z++){
-        for (int x=0;x<recuentoancho;x++){  //recorrer imagen
-            for (int y=0;y<recuentoalto;y++){
-                R=R+im.pixelColor(x+(columna*recuentoalto),y+(renglon*recuentoancho)).red();
-                G=G+im.pixelColor(x+(columna*recuentoalto),y+(renglon*recuentoancho)).green();
-                B=B+im.pixelColor(x+(columna*recuentoalto),y+(renglon*recuentoancho)).blue();
-
+    for(int z=0; z<anchomatrix;z++){
+        for(int w=0; w<altomatrix; w++){
+            for(int x=0+(z*recuentoalto); x<recuentoalto+(z*recuentoalto);x++){
+                for(int y=0+(w*recuentoancho);y<recuentoancho+(w*recuentoancho);y++){
+                    R=R+im.pixelColor(x,y).red();
+                    G=G+im.pixelColor(x,y).green();
+                    B=B+im.pixelColor(x,y).blue();
+                }
             }
-        renglon+=1;
+            matrixrojo[z][w]=R/(recuentoancho*recuentoalto);
+            matrixverde[z][w]=G/(recuentoancho*recuentoalto);
+            matrixazul[z][w]=B/(recuentoancho*recuentoalto);
+            R=0;
+            G=0;
+            B=0;
         }
-
-     if (renglon==altomatrix){
-             columna+=1;
-             renglon=0;
-     }
     }
+   for (int h=0;h<altomatrix;h++){
+       for (int sopa=0;sopa<anchomatrix;sopa++){
+           cout<<matrixazul[h][sopa];
+       }
+       cout<<endl;
+   }
+
     return 0;
 }
 
-//cout<<"intensidad del rojo: "<<im.pixelColor(x,y).red()<<endl;
-//cout<<"intensidad del verde: "<<im.pixelColor(x,y).green()<<endl;
-//cout<<"intensidad del azul: "<<im.pixelColor(x,y).blue()<<endl;
-//a+=1;
